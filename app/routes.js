ea449320be5713d9b0db0a7984a539cbc279ea43
J S Johal <JSJohal@users.noreply.github.com>
2019-11-26 14:19:46 +0000

    adding example work based

--- app/routes.js
+++ app/routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 require('./routes/sprint9/routing.js')(router);
+require('./routes/sprint28/routing.js')(router);
 
 module.exports = router
